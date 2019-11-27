1ba681f630402d969f62d4367428ec25c8e7fe4a
Romain  BOISSON <romain.nicolas.boisson@gmail.com>
2019-11-27 16:48:00 +0100

    transformation en RestAPI et suppression des templates

--- src/main/java/PretPres/Controllers/GreetingController.java
+++ src/main/java/PretPres/Controllers/GreetingController.java
@@ -1,18 +1,21 @@
 package PretPres.Controllers;
 
+import PretPres.Models.Greeting;
+import jdk.security.jarsigner.JarSigner;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 
     @RequestMapping("")
-    public String greeting(@RequestParam(value="name", defaultValue="World") String name,Model model) {
-        model.addAttribute("name", name);
-        System.out.println("here");
-        return "greetingView";
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name, Model model) {
+
+        return new Greeting(1,"test");
     }
 }
